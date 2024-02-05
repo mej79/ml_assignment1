56ab779a190aa6e3865d98fff7d5009bb6e46970
mej79 <max_e_g_jahnke@yahoo.com>
2024-02-05 14:14:31 -0500

    Added Visualizations

--- assignment1.py
+++ assignment1.py
@@ -1,4 +1,5 @@
 import warnings
+from matplotlib import pyplot as plt
 from sklearn.exceptions import ConvergenceWarning
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import tree
@@ -16,8 +17,9 @@ def Classify(clf, folds):
     for fold in range(num_folds):
         mask = np.zeros(num_folds)
         mask[fold] = True
-        training = np.ma.masked_array(folds, mask=mask)
-        training = np.concatenate(training[~training.mask])
+        training = np.copy(folds)
+        np.delete(training, fold, 0)
+        training = np.concatenate(training)
         validation = folds[fold]
         X = np.array(training[:, :-1])
         y = np.array(training[:, -1:].flatten())
@@ -34,7 +36,7 @@ def Classify(clf, folds):
     return best_model
 
 def Folds(data_location, dtype=int, train_percent=80, num_folds=10):
-    dataframe = genfromtxt(data_location, delimiter=',', dtype=dtype)[1:]
+    dataframe = genfromtxt(data_location, delimiter=',', dtype=dtype)[1:, 1:]
     train_index = round(len(dataframe) * train_percent / 100)
     training, test = dataframe[:train_index], dataframe[train_index:]
 
@@ -54,12 +56,21 @@ def Test(model_perf, test):
         for i in range(len(predictions)):
             if predictions[i] == test[i, -1]:
                 correct_count += 1
-        test_perf = correct_count / len(test)
+        test_error = 1 - correct_count / len(test)
 
-        final_output.append([item[0], test_perf])
+        final_output.append([item[0], test_error])
 
 
-    return final_output
+    return np.asarray(final_output)
+
+def PlotError(model, data):
+    x = data[:,0]
+    y = data[:,1]
+
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+    ax.set_ylim(bottom=0)
+    ax.set_title(model)
 
 def KNN(folds, k_min=1, k_max=5):
     model_perf = []
@@ -102,7 +113,7 @@ def SVM(folds, iter_min=1, iter_max=5, function='rbf'):
         svm_clf = SVC(random_state=0, max_iter=iter, kernel=function)
         model_perf.append([iter, Classify(clf=svm_clf, folds=folds)])
 
-    return model_perf
+    return (model_perf)
 
 
 ###########################################################################################################
@@ -112,16 +123,42 @@ def SVM(folds, iter_min=1, iter_max=5, function='rbf'):
 with warnings.catch_warnings():
     warnings.filterwarnings('ignore', category=VisibleDeprecationWarning)
     warnings.filterwarnings('ignore', category=ConvergenceWarning)
-    folds, test = Folds('data/breast-cancer-dataset.csv')
-    knn_output = KNN(folds=folds)
-    print(Test(knn_output, test=test))
-    dt_output = DecisionTree(folds=folds)
-    print(Test(dt_output, test=test))
-    boosted_output = BoostedDecisionTree(folds=folds)
-    print(Test(boosted_output, test=test))
-    mlp_output = NeuralNet(folds=folds)
-    print(Test(mlp_output, test=test))
-    svm_output = SVM(folds=folds)
-    print(Test(svm_output, test=test))
-    svm_output = SVM(folds=folds, function='poly')
-    print(Test(svm_output, test=test))
+    folds, test = Folds('data/apple_quality.csv')
+    # print(folds)
+    print('Starting KNN')
+    knn_output = KNN(folds=folds, k_max=25)
+    knn_test_results = Test(knn_output, test=test)
+    print('Done')
+    # print(knn_test_results)
+    print('Starting DT')
+    dt_output = DecisionTree(folds=folds, depth_max=20)
+    dt_test_results = Test(dt_output, test=test)
+    print('Done')
+    # print(dt_test_results)
+    print('Starting Boosted DT')
+    boosted_output = BoostedDecisionTree(folds=folds, estimators_max=25)
+    boosted_test_results = Test(boosted_output, test=test)
+    print('Done')
+    # print(boosted_test_results)
+    print('Starting Neural Nets')
+    mlp_output = NeuralNet(folds=folds, iter_max=25)
+    mlp_test_results = Test(mlp_output, test=test)
+    print('Done')
+    # print(mlp_test_results)
+    print('Starting SVM')
+    svm_output = SVM(folds=folds, iter_max=25)
+    svm_test_results = Test(svm_output, test=test)
+    print('Done')
+    # print(svm_test_results)
+    # svm_output = SVM(folds=folds, function='poly')
+    # print(Test(svm_output, test=test))
+
+
+    PlotError('KNN', knn_test_results)
+    PlotError('Decision Trees', dt_test_results)
+    PlotError('Boosted Decision Trees', boosted_test_results)
+    PlotError('Neural Network', mlp_test_results)
+    PlotError('Support Vector Machine', svm_test_results)
+
+
+    plt.show()
